e016b6062c8b09ad96fe51d069bf86cef93f1f80
web3ten0 <web3ten0@gmail.com>
2023-03-01 16:06:46 +0900

    added user and post

--- script.ts
+++ script.ts
@@ -3,8 +3,17 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const users = await prisma.user.findMany();
-  console.log(users);
+  const user = await prisma.user.create({
+    data: {
+      name: "Jiro",
+      email: "jiro@hoge.com",
+      posts: {
+        create: {
+          title: "hello world",
+        },
+      },
+    },
+  });
 }
 
 main()
